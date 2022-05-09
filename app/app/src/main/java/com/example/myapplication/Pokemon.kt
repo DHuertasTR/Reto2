f51ae53bebf2d68b2408f6990be2a7ddaf2e4ae2
david huertas <aletru321@gmail.com>
2022-05-08 22:06:25 -0500

    add data clases user and pokemon

---
+++ app/app/src/main/java/com/example/myapplication/Pokemon.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import java.util.*
+
+data class Pokemon(
+    var id:String,
+    var name:String,
+    var captureDate:Date,
+    var type:String,
+    var defense:String,
+    var atack:String,
+    var velocity:String,
+    var life:String
+)
---
+++ app/app/src/main/java/com/example/myapplication/User.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+data class User(
+    var username:String,
+    var array: Array<String>
+)
